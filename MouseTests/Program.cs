2a9635b1d0f37a86b337867bfb65effb30691adb
BDisp <bd.bdisp@gmail.com>
2026-03-16 21:40:47 +0000

    Add more mouse flags with last and current flags

--- MouseTests/Program.cs
+++ MouseTests/Program.cs
@@ -15,6 +15,13 @@ class Program
     // Define mouse button state constants
     private const uint FROM_LEFT_1ST_BUTTON_PRESSED = 0x0001;
     private const uint RIGHTMOST_BUTTON_PRESSED = 0x0002;
+    private const uint FROM_LEFT_2ND_BUTTON_PRESSED = 0x0004;
+
+    // Define mouse event flags
+    private const uint MOUSE_MOVED = 0x0001;
+    private const uint DOUBLE_CLICK = 0x0002;
+    private const uint MOUSE_WHEELED = 0x0004;
+    private const uint MOUSE_HWHEELED = 0x0008;
 
     // Input record structure
     [StructLayout(LayoutKind.Explicit)]
@@ -77,7 +84,10 @@ class Program
 
         INPUT_RECORD[] records = new INPUT_RECORD[1];
         uint numRead;
+        uint lastMouseEventFlags = 0;
+        COORD lastMousePosition = new();
 
+        Console.Clear();
         Console.WriteLine("Mouse events capture started. Press ESC to exit.");
 
         while (true)
@@ -89,34 +99,68 @@ class Program
                 {
                     var mouseEvent = records[0].MouseEvent;
 
-                    // Safely move the cursor to the previous line if possible
-                    int currentLine = Console.CursorTop;
-                    if (currentLine > 1)
+                    SetCurrentLine(1);
+                    Console.Write($"lastMouseEventFlags: ");
+                    switch (lastMouseEventFlags)
                     {
-                        Console.SetCursorPosition(0, 1);
-                    }
-                    else
-                    {
-                        Console.SetCursorPosition(0, currentLine); // Stay on the current line
+                        case 0:
+                            Console.WriteLine($"Mouse released at ({lastMousePosition.X}, {lastMousePosition.Y})");
+                            break;
+                        case MOUSE_MOVED:
+                            Console.WriteLine($"Mouse moved to ({lastMousePosition.X}, {lastMousePosition.Y})");
+                            break;
+                        case DOUBLE_CLICK:
+                            Console.WriteLine($"Mouse double click at ({lastMousePosition.X}, {lastMousePosition.Y})");
+                            break;
+                        case MOUSE_WHEELED:
+                            Console.WriteLine($"Mouse vertical wheeled at ({lastMousePosition.X}, {lastMousePosition.Y})");
+                            break;
+                        case MOUSE_HWHEELED:
+                            Console.WriteLine($"Mouse horizontal wheeled at ({lastMousePosition.X}, {lastMousePosition.Y})");
+                            break;
                     }
 
-                    // Clear the current line
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(0, currentLine > 1 ? 1 : currentLine);
+                    SetCurrentLine(2);
+                    Console.Write($"currentMouseEventFlags: ");
+                    switch (mouseEvent.dwButtonState)
+                    {
+                        // Handle mouse button presses
+                        case FROM_LEFT_1ST_BUTTON_PRESSED:
+                            Console.WriteLine($"Left button pressed at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                            break;
+                        case RIGHTMOST_BUTTON_PRESSED:
+                            Console.WriteLine($"Right button pressed at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                            break;
+                        case FROM_LEFT_2ND_BUTTON_PRESSED:
+                            Console.WriteLine($"Middle button pressed at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                            break;
+                        default:
+                        {
+                            switch (mouseEvent.dwEventFlags)
+                            {
+                                case 0:
+                                    Console.WriteLine($"Button released at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                                    break;
+                                case MOUSE_MOVED:
+                                    Console.WriteLine($"Mouse moved to ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                                    break;
+                                case DOUBLE_CLICK:
+                                    Console.WriteLine($"Mouse double click at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                                    break;
+                                case MOUSE_WHEELED:
+                                    Console.WriteLine($"Mouse vertical wheeled at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                                    break;
+                                case MOUSE_HWHEELED:
+                                    Console.WriteLine($"Mouse horizontal wheeled at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                                    break;
+                            }
 
-                    // Handle mouse button presses
-                    if (mouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
-                    {
-                        Console.WriteLine($"Left button pressed at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
+                            break;
+                        }
                     }
-                    else if (mouseEvent.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
-                    {
-                        Console.WriteLine($"Right button pressed at ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
-                    }
-                    else if (mouseEvent.dwEventFlags == 0)
-                    {
-                        Console.WriteLine($"Mouse moved to ({mouseEvent.dwMousePosition.X}, {mouseEvent.dwMousePosition.Y})");
-                    }
+
+                    lastMouseEventFlags = mouseEvent.dwEventFlags;
+                    lastMousePosition = mouseEvent.dwMousePosition;
                 }
                 else if (records[0].EventType == KEY_EVENT)
                 {
@@ -136,5 +180,23 @@ class Program
                 }
             }
         }
+    }
+
+    private static void SetCurrentLine(int line)
+    {
+        // Safely move the cursor to the previous line if possible
+        int currentLine = Console.CursorTop;
+        if (currentLine > line)
+        {
+            Console.SetCursorPosition(0, line);
+        }
+        else
+        {
+            Console.SetCursorPosition(0, currentLine); // Stay on the current line
+        }
+
+        // Clear the current line
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, currentLine > line ? line : currentLine);
     }
 }
